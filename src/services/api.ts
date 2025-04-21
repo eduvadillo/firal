29741309c9197f43a20d868a871cbe2b6f5bc186
eduvadi <eduvadillo1@gmail.com>
2025-04-21 15:14:57 +0200

    refactor: api services

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
+const baseURL = process.env.REACT_APP_API_URL
+  ? `${process.env.REACT_APP_API_URL}/api`
+  : 'http://localhost:5000/api';
+
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  withCredentials: true, // This is important for cookies
+  baseURL,
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
 });
 
 export default api;
